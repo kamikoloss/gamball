9b6bc88c28bb931addac339abb94f749493f968d
Kamiko Loss <info@gloxi.net>
2024-10-23 03:32:09 +0900

    Shop の UI を作成した

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -5,22 +5,25 @@ extends RigidBody2D
 # { <Level>: ボールの色 } 
 const BALL_COLORS = {
 	-1: Color(0.5, 0.5, 0.5, 0.5),
-	0: Color.GRAY, 1: Color.YELLOW, 2: Color.BLUE, 3: Color.RED,
+	0: Color(0.875, 0.875, 0.875), 1: Color.YELLOW, 2: Color.BLUE, 3: Color.RED,
 	4: Color.PURPLE, 5: Color.ORANGE, 6: Color.GREEN, 7: Color.SADDLE_BROWN,
-	8: Color.BLACK, 9: Color.GOLD, 10: Color.LIGHT_BLUE, 11: Color.LIGHT_CORAL,
+	8: Color(0.125, 0.125, 0.125), 9: Color.GOLD, 10: Color.LIGHT_BLUE, 11: Color.LIGHT_CORAL,
 	12: Color.LIGHT_SALMON, 13: Color.LIGHT_SALMON, 14: Color.LIGHT_GREEN, 15: Color.ROSY_BROWN,
 }
 
 
-@export var level: int = 0
+@export var level: int = 0 # ボール番号
+@export var is_display: bool = false # 展示用のボールかどうか
 
 @export var _texture_rect: TextureRect
 @export var _texture_rect_in: TextureRect
 @export var _label: Label
+@export var _area2d: Area2D
 
 
 func _ready() -> void:
 	refresh_view()
+	refresh_display()
 
 
 # 自身の見た目を更新する
@@ -31,3 +34,11 @@ func refresh_view() -> void:
 		_texture_rect_in.visible = false
 	else:
 		_label.text = str(level)
+
+
+# 自身の展示設定を更新する
+func refresh_display() -> void:
+	if is_display:
+		freeze = true
+		collision_layer = 0
+		_area2d.monitoring = false
