f6984f1ec83ad35750eb3ee1341ed0fb7eb19ea5
Kamiko Loss <info@gloxi.net>
2024-11-03 15:42:33 +0900

    game の UI 部分を game_ui に切り分けた

--- scripts/game.gd
+++ scripts/game.gd
@@ -23,18 +23,10 @@ const TAX_LIST = [
 	[100, TaxType.Money, 800], # Balls 400
 	[125, TaxType.Balls, 600],
 	[150, TaxType.Balls, 800],
-	[175, TaxType.Money, 2000], # Balls 1000
+	[175, TaxType.Balls, 1000], # Balls 1000
 ]
 # ゲームクリアになる Turn
 const CLEAR_TURN: int = 200
-
-# TODO: UI
-const UI_POSITION_BOTTOM_FROM: Vector2 = Vector2(0, 720)
-const UI_POSITION_BOTTOM_TO: Vector2 = Vector2(0, -720)
-const UI_POSITION_RIGHT_FROM: Vector2 = Vector2(1280, 0)
-const UI_POSITION_RIGHT_TO: Vector2 = Vector2(720, 0)
-const UI_POSITION_TO: Vector2 = Vector2(0, 0)
-const UI_MOVE_DURATION: float = 1.0
 
 
 # PackedScene
@@ -46,51 +38,24 @@ const UI_MOVE_DURATION: float = 1.0
 @export var _pachinko: Pachinko
 @export var _stack: Stack
 @export var _balls: Node2D # Ball instances の親 Node
-
-# TODO: クラス分ける
-# UI Main
-@export var _buy_balls_button: Button
-@export var _sell_balls_button: Button
-@export var _info_button: Button
-@export var _turn_label: Label
-@export var _money_label: Label
-@export var _balls_label: Label
-@export var _payout_label: Label
-@export var _next_turn_label: Label
-@export var _next_type_label: Label
-@export var _next_amount_label: Label
-@export var _balls_slot_deck: Control
-@export var _balls_slot_extra: Control
-# UI Main Arrow
-@export var _arrow: Control
-@export var _arrow_square: TextureRect
-@export var _drag_point: TextureRect
-# UI Tax
-@export var _ui_tax: Control
-@export var _tax_pay_button: TextureButton
-# UI Shop
-@export var _ui_shop: Control
-@export var _products: Control
-@export var _shop_exit_button: TextureButton
-# UI People
-@export var _ui_people: Control
+@export var _products: Control # Product の親
+@export var _game_ui: GameUi
 
 
 # TODO: Autoload に置いていい気がする
 var turn: int = 0:
 	set(value):
 		turn = value
-		_turn_label.text = str(turn)
-		_refresh_next()
+		_game_ui.refresh_turn_label(value)
 var money: int = 0:
 	set(value):
 		money = value
-		_money_label.text = str(money)
+		_game_ui.refresh_money_label(value)
 		_set_money_to_products()
 var balls: int = 0:
 	set(value):
 		balls = value
-		_balls_label.text = str(balls)
+		_game_ui.refresh_balls_label(value)
 
 
 # 現在ドラッグ中かどうか
@@ -103,11 +68,11 @@ var _drag_position_to: Vector2
 # NOTE: _drag_length_max と反比例させる
 var _impulse_ratio: float = 10
 
-# 出現する Deck Ball level のリスト (確率込み)
+# 出現する Deck Ball level の初期リスト (確率込み)
 var _deck_level_list: Array[int] = [0, 0, 0, 0, 0, 1, 1, 1, 2, 2]
-# 出現する Extra Ball level のリスト (確率込み)
+# 出現する Extra Ball level の初期リスト (確率込み)
 var _extra_level_list: Array[int] = [5, 6, 7, 8, 9]
-# 払い出しが残っている Ball level のリスト
+# 払い出しキューが残っている Ball level のリスト
 var _payout_level_list: Array[int] = []
 # 何秒ごとに 1 Ball を払い出すか
 # NOTE: Stack の排出速度を見ていい感じに調整する
@@ -124,52 +89,39 @@ var _sell_rate: Array[int] = [50, 100]
 var _next_tax_index = 0
 
 
-
 func _ready() -> void:
-	# UI
-	_refresh_balls_slot_deck()
-	_refresh_balls_slot_extra()
-	# Arrow
-	_arrow.visible = false
-	_arrow_square.scale.y = 0
-	_drag_point.visible = false
-
-	# Label 用に初期化する
+	# 初期化
 	turn = 0
 	money = 1000
 	balls = 0
 
-	# Button に接続する
-	_buy_balls_button.pressed.connect(_on_buy_balls_button_pressed)
-	_sell_balls_button.pressed.connect(_on_sell_balls_button_pressed)
-	_tax_pay_button.pressed.connect(_on_tax_pay_button_pressed)
-	_shop_exit_button.pressed.connect(_on_shop_exit_button_pressed)
-	_info_button.pressed.connect(_on_info_button_pressed)
-	# すべての Hole の signal に接続する
-	var holes = get_tree().get_nodes_in_group("hole")
-	for hole: Hole in holes:
-		hole.ball_entered.connect(_on_hole_ball_entered)
-	# ビリヤード盤面上の入力を処理する
+	# Input
 	_billiards_board.input_event.connect(_on_billiards_board_input)
 
-	# すべての Product の signal に接続する
+	# Signal
+	_game_ui.buy_balls_button_pressed.connect(_on_buy_balls_button_pressed)
+	_game_ui.sell_balls_button_pressed.connect(_on_sell_balls_button_pressed)
+	_game_ui.tax_pay_button_pressed.connect(_on_tax_pay_button_pressed)
+	_game_ui.shop_exit_button_pressed.connect(_on_shop_exit_button_pressed)
+	_game_ui.info_button_pressed.connect(_on_info_button_pressed)
+	# Signal (Hole)
+	for maybe_hole in get_tree().get_nodes_in_group("hole"):
+		if maybe_hole is Hole:
+			maybe_hole.ball_entered.connect(_on_hole_ball_entered)
+	# Signal (Product)
 	for maybe_product in _products.get_children():
 		if maybe_product is Product:
 			maybe_product.icon_pressed.connect(_on_product_icon_pressed)
+
+	# GameUi
+	_game_ui.refresh_balls_slot_deck(_deck_level_list)
+	_game_ui.refresh_balls_slot_extra(_extra_level_list)
+	_refresh_next()
 
 	# ボール購入ボタンを1プッシュする
 	_on_buy_balls_button_pressed()
 	# 払い出し処理を開始する
 	_start_payout()
-
-
-# Ball instance を作成する
-func create_new_ball(level: int = 0, is_active = true) -> Ball:
-	var ball: Ball = _ball_scene.instantiate()
-	ball.level = level
-	ball.is_active = is_active
-	_balls.add_child(ball)
-	return ball
 
 
 # 左クリックを押したとき: Billiards Board 上に限定する
@@ -179,12 +131,11 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		# 左クリックを離したとき
 		if not event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			# ドラッグ関連
 			_is_dragging = false
-			# Arrow
-			_arrow.visible = false
-			_arrow_square.scale.y = 0
-			_drag_point.visible = false
+			
+			# GameUi
+			_game_ui.hide_drag_point()
+			_game_ui.hide_arrow()
 
 			# ドラッグの距離を算出して丸める
 			var drag_vector = Vector2.ZERO
@@ -208,7 +159,11 @@ func _input(event: InputEvent) -> void:
 		# ドラッグしている間
 		if _is_dragging:
 			_drag_position_to = event.position
-			_refresh_arrow()
+			var drag_vector = _drag_position_from - _drag_position_to
+			var clamped_length =  clampf(drag_vector.length(), 0, DRAG_LENGTH_MAX)
+			var deg = rad_to_deg(drag_vector.angle()) + 90
+			var scale = (clamped_length / DRAG_LENGTH_MAX) * 10 # scale 10 が最大
+			_game_ui.refresh_arrow(deg, scale)
 
 
 func _on_buy_balls_button_pressed() -> void:
@@ -229,7 +184,6 @@ func _on_sell_balls_button_pressed() -> void:
 	money += money_unit
 
 
-
 func _on_tax_pay_button_pressed() -> void:
 	var next_turn = TAX_LIST[_next_tax_index][0]
 	var next_type = TAX_LIST[_next_tax_index][1]
@@ -241,13 +195,16 @@ func _on_tax_pay_button_pressed() -> void:
 		balls -= next_amount
 
 	_next_tax_index += 1
-	_refresh_next()
-	_hide_tax_window()
-	_show_shop_window()
+
+
+
+	_game_ui.hide_tax_window()
+	_game_ui.show_shop_window()
 
 
 func _on_shop_exit_button_pressed() -> void:
-	_hide_shop_window()
+	_refresh_next()
+	_game_ui.hide_shop_window()
 
 
 func _on_info_button_pressed() -> void:
@@ -263,12 +220,12 @@ func _on_hole_ball_entered(hole: Hole, ball: Ball) -> void:
 			if not ball.is_active:
 				return
 			# パチンコ盤面上に同じ Ball を出現させる
-			var new_ball = create_new_ball(ball.level)
+			var new_ball = _create_new_ball(ball.level)
 			_pachinko.spawn_ball(new_ball)
 		Hole.HoleType.Extra:
 			# ビリヤード盤面上にランダムな Extra Ball を出現させる
 			var level = _extra_level_list.pick_random()
-			var new_ball = create_new_ball(level)
+			var new_ball = _create_new_ball(level)
 			_billiards.spawn_extra_ball(new_ball)
 		Hole.HoleType.Gain:
 			# 払い出しリストに追加する
@@ -289,18 +246,18 @@ func _on_billiards_board_input(viewport: Node, event: InputEvent, shape_idx: int
 		# 左クリックを押したとき
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if 0 < balls:
-				# ドラッグ関連
 				_is_dragging = true
 				_drag_position_from = event.position
 				_drag_position_to = Vector2.ZERO
-				# Arrow
-				_arrow.visible = true
-				_drag_point.visible = true
-				_drag_point.position = _drag_position_from
+
+				# GameUi
+				_game_ui.show_drag_point(_drag_position_from)
+				_game_ui.show_arrow()
+
 				# ビリヤード盤面上に Ball を生成する
 				balls -= 1
 				var level = _deck_level_list.pick_random()
-				var new_ball = create_new_ball(level, false) # 最初の出現時には有効化されていない
+				var new_ball = _create_new_ball(level, false) # 最初の出現時には有効化されていない
 				_billiards.spawn_ball(new_ball)
 				# 1ターン進める
 				# Ball 生成をなかったことにしてもこれはなかったことにはしない
@@ -356,8 +313,17 @@ func _on_product_icon_pressed(product: Product) -> void:
 	money -= product.price
 
 	# DECK, EXTRA の見た目を更新する
-	_refresh_balls_slot_deck()
-	_refresh_balls_slot_extra()
+	_game_ui.refresh_balls_slot_deck(_deck_level_list)
+	_game_ui.refresh_balls_slot_extra(_extra_level_list)
+
+
+# Ball instance を作成する
+func _create_new_ball(level: int = 0, is_active = true) -> Ball:
+	var ball: Ball = _ball_scene.instantiate()
+	ball.level = level
+	ball.is_active = is_active
+	_balls.add_child(ball)
+	return ball
 
 
 # 重み付きのレア度を抽選する
@@ -390,66 +356,37 @@ func _pick_random_rarity() -> Ball.Rarity:
 	return random_rarity
 
 
+# Payout のループを開始する
 func _start_payout() -> void:
 	var tween = create_tween()
 	tween.set_loops()
 	tween.tween_callback(_pop_payout).set_delay(_payout_interval)
 
+# 払い出しキューに追加する
 func _push_payout(level: int, amount: int) -> void:
 	for i in amount:
 		_payout_level_list.push_back(level)
-	_payout_label.text = str(_payout_level_list.size())
+	_game_ui.refresh_payout_label(_payout_level_list.size())
 
+# 払い出しキューを実行する
 func _pop_payout() -> void:
 	if _payout_level_list.is_empty():
 		return
 	var level = _payout_level_list.pop_front()
-	var new_ball = create_new_ball(level)
+	var new_ball = _create_new_ball(level)
 	_stack.spawn_ball(new_ball)
-	_payout_label.text = str(_payout_level_list.size())
+	_game_ui.refresh_payout_label(_payout_level_list.size())
 
 
-func _refresh_arrow() -> void:
-	var drag_vector = _drag_position_from - _drag_position_to
-	var clamped_length =  clampf(drag_vector.length(), 0, DRAG_LENGTH_MAX)
-	_arrow.rotation_degrees = rad_to_deg(drag_vector.angle()) + 90
-	_arrow_square.scale.y = (clamped_length / DRAG_LENGTH_MAX) * 10 # scale 10 が最大
-
-
-func _refresh_balls_slot_deck() -> void:
-	_refresh_balls_slot(_balls_slot_deck, _deck_level_list)
-
-func _refresh_balls_slot_extra() -> void:
-	_refresh_balls_slot(_balls_slot_extra, _extra_level_list)
-
-func _refresh_balls_slot(parent_node: Node, level_list: Array[int]) -> void:
-	var index = 0
-	for node in parent_node.get_children():
-		if node is Ball: # Label もある
-			if index < level_list.size():
-				node.level = level_list[index]
-			else:
-				node.level = Ball.BALL_LEVEL_EMPTY_SLOT
-			node.refresh_view()
-			index += 1
-
-
+# Next 関連の見た目を更新する
 func _refresh_next() -> void:
-	# クリアしたとき
 	if CLEAR_TURN <= turn:
-		_next_turn_label.text = ""
-		_next_type_label.text = ""
-		_next_amount_label.text = "CLEAR!!"
-		return
-
-	var next_turn = TAX_LIST[_next_tax_index][0]
-	var next_type = TAX_LIST[_next_tax_index][1]
-	var next_type_text = "￥" if next_type == TaxType.Money else "●"
-	var next_amount = TAX_LIST[_next_tax_index][2]
-
-	_next_turn_label.text = str(next_turn)
-	_next_type_label.text = next_type_text
-	_next_amount_label.text = "-%s" % [next_amount]
+		_game_ui.refresh_next_clear()
+	else:
+		var turn = TAX_LIST[_next_tax_index][0]
+		var type = TAX_LIST[_next_tax_index][1]
+		var amount = TAX_LIST[_next_tax_index][2]
+		_game_ui.refresh_next(turn, type, amount)
 
 
 # 1ターン進める
@@ -459,48 +396,7 @@ func _go_to_next_turn() -> void:
 	if CLEAR_TURN <= turn:
 		return
 	if turn == TAX_LIST[_next_tax_index][0]:
-		_show_tax_window()
-
-
-func _show_tax_window() -> void:
-	_show_people_window()
-	_ui_tax.visible = true
-	_ui_tax.position = UI_POSITION_BOTTOM_FROM
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(_ui_tax, "position", UI_POSITION_TO, UI_MOVE_DURATION)
-
-func _hide_tax_window() -> void:
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(_ui_tax, "position", UI_POSITION_BOTTOM_TO, UI_MOVE_DURATION)
-
-
-func _show_shop_window() -> void:
-	_ui_shop.visible = true
-	_ui_shop.position = UI_POSITION_BOTTOM_FROM
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(_ui_shop, "position", UI_POSITION_TO, UI_MOVE_DURATION)
-
-func _hide_shop_window() -> void:
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(_ui_shop, "position", UI_POSITION_BOTTOM_TO, UI_MOVE_DURATION)
-	_hide_people_window()
-
-
-func _show_people_window() -> void:
-	_ui_people.visible = true
-	_ui_people.position = UI_POSITION_RIGHT_FROM
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(_ui_people, "position", UI_POSITION_TO, UI_MOVE_DURATION)
-
-func _hide_people_window() -> void:
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(_ui_people, "position", UI_POSITION_RIGHT_TO, UI_MOVE_DURATION)
+		_game_ui.show_tax_window()
 
 
 # TODO: Autoload にしたらいらなくなる
--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,12 +1,168 @@
-class_name GameUI
+class_name GameUi
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+signal buy_balls_button_pressed
+signal sell_balls_button_pressed
+signal tax_pay_button_pressed
+signal shop_exit_button_pressed
+signal info_button_pressed
+
+
+const WINDOW_POSITION_BOTTOM_FROM: Vector2 = Vector2(0, 720)
+const WINDOW_POSITION_BOTTOM_TO: Vector2 = Vector2(0, -720)
+const WINDOW_POSITION_RIGHT_FROM: Vector2 = Vector2(1280, 0)
+const WINDOW_POSITION_RIGHT_TO: Vector2 = Vector2(720, 0)
+const WINDOW_POSITION_TO: Vector2 = Vector2(0, 0)
+const WINDOW_MOVE_DURATION: float = 1.0
+
+@export_category("Main/BallsSlotXxxx")
+@export var _balls_slot_deck: Control
+@export var _balls_slot_extra: Control
+@export_category("Main/Buttons")
+@export var _buy_balls_button: Button
+@export var _sell_balls_button: Button
+@export var _info_button: Button
+@export_category("Main/Score")
+@export var _turn_label: Label
+@export var _money_label: Label
+@export var _payout_label: Label
+@export var _balls_label: Label
+@export var _next_turn_label: Label
+@export var _next_type_label: Label
+@export var _next_amount_label: Label
+@export_category("Main/DragPoint")
+@export var _drag_point: Control
+@export_category("Main/Arrow")
+@export var _arrow: Control
+@export var _arrow_square: TextureRect
+
+@export_category("Tax")
+@export var _tax_window: Control
+@export var _tax_pay_button: TextureButton
+@export var _tax_give_up_button: TextureButton
+
+@export_category("Shop")
+@export var _shop_window: Control
+@export var _shop_exit_button: TextureButton
+
+@export_category("People")
+@export var _people_window: Control
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	# Main/Buttons
+	_buy_balls_button.pressed.connect(func(): buy_balls_button_pressed.emit())
+	_sell_balls_button.pressed.connect(func(): sell_balls_button_pressed.emit())
+	_tax_pay_button.pressed.connect(func(): tax_pay_button_pressed.emit())
+	_shop_exit_button.pressed.connect(func(): shop_exit_button_pressed.emit())
+	_info_button.pressed.connect(func(): info_button_pressed.emit())
+	# Main/DragPoint
+	hide_drag_point()
+	# Main/Arrow
+	hide_arrow()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func show_drag_point(position: Vector2) -> void:
+	_drag_point.visible = true
+	_drag_point.position = position
+
+func hide_drag_point() -> void:
+	_drag_point.visible = false
+
+
+func show_arrow() -> void:
+	_arrow.visible = true
+
+func hide_arrow() -> void:
+	_arrow.visible = false
+	_arrow_square.scale.y = 0
+	_drag_point.visible = false
+
+func refresh_arrow(deg: int, scale: float) -> void:
+	_arrow.rotation_degrees = deg
+	_arrow_square.scale.y = scale
+
+
+func show_tax_window() -> void:
+	show_people_window() # 連動
+	_tax_window.visible = true
+	_tax_window.position = WINDOW_POSITION_BOTTOM_FROM
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_tax_window, "position", WINDOW_POSITION_TO, WINDOW_MOVE_DURATION)
+
+func hide_tax_window() -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_tax_window, "position", WINDOW_POSITION_BOTTOM_TO, WINDOW_MOVE_DURATION)
+
+
+func show_shop_window() -> void:
+	_shop_window.visible = true
+	_shop_window.position = WINDOW_POSITION_BOTTOM_FROM
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_shop_window, "position", WINDOW_POSITION_TO, WINDOW_MOVE_DURATION)
+
+func hide_shop_window() -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_shop_window, "position", WINDOW_POSITION_BOTTOM_TO, WINDOW_MOVE_DURATION)
+	hide_people_window() # 連動
+
+
+func show_people_window() -> void:
+	_people_window.visible = true
+	_people_window.position = WINDOW_POSITION_RIGHT_FROM
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_people_window, "position", WINDOW_POSITION_TO, WINDOW_MOVE_DURATION)
+
+func hide_people_window() -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_people_window, "position", WINDOW_POSITION_RIGHT_TO, WINDOW_MOVE_DURATION)
+
+
+func refresh_balls_slot_deck(deck_level_list: Array[int]) -> void:
+	_refresh_balls_slot(_balls_slot_deck, deck_level_list)
+
+func refresh_balls_slot_extra(extra_level_list: Array[int]) -> void:
+	_refresh_balls_slot(_balls_slot_extra, extra_level_list)
+
+
+func refresh_turn_label(turn: int) -> void:
+	_turn_label.text = str(turn)
+	
+func refresh_money_label(money: int) -> void:
+	_money_label.text = str(money)
+
+func refresh_payout_label(payout: int) -> void:
+	_payout_label.text = str(payout)
+
+func refresh_balls_label(balls: int) -> void:
+	_balls_label.text = str(balls)
+
+
+func refresh_next(turn: int, type: Game.TaxType, amount: int) -> void:
+	_next_turn_label.text = str(turn)
+	_next_type_label.text = "￥" if type == Game.TaxType.Money else "●"
+	_next_amount_label.text = str(amount)
+
+func refresh_next_clear() -> void:
+	_next_turn_label.text = ""
+	_next_type_label.text = ""
+	_next_amount_label.text = "CLEAR!!"
+
+
+func _refresh_balls_slot(parent_node: Node, level_list: Array[int]) -> void:
+	var index = 0
+	for node in parent_node.get_children():
+		if node is Ball: # Label もある
+			if index < level_list.size():
+				node.level = level_list[index]
+			else:
+				node.level = Ball.BALL_LEVEL_EMPTY_SLOT
+			node.refresh_view()
+			index += 1
