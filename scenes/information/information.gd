f95cf0f397c10b281e4e2d82b945cbc4417d8bbf
Kamiko Loss <info@gloxi.net>
2024-11-27 23:51:10 +0900

    Theme 差し替え - Information

--- scenes/information/information.gd
+++ scenes/information/information.gd
@@ -5,10 +5,10 @@ extends Control
 signal exit_button_pressed
 
 
-@export var _exit_button: TextureButton
+@export var _exit_button: Button
 
 @export var _balls_parent: Control
-@export var _ball_popup_level: Label
+@export var _ball_popup_title: Label
 @export var _ball_popup_description: RichTextLabel
 
 
@@ -28,24 +28,23 @@ func _init_display_balls() -> void:
 			node.refresh_view()
 			node.hovered.connect(func(entered): _show_ball_popup(node) if entered else _hide_ball_popup(node))
 			level += 1
-	_ball_popup_level.text = ""
+	_ball_popup_title.text = ""
 	_ball_popup_description.text = ""
 
 
 # Ball の効果をレアリティごとにすべて表示する
 func _show_ball_popup(ball: Ball) -> void:
 	ball.show_hover()
-	_ball_popup_level.text = str(ball.level)
 
-	var description = " \n"
+	_ball_popup_title.text = "No.%s" % ball.level
+	var description = "[font_size=8] [/font_size]\n"
 	for rarity in Ball.Rarity.values():
-		var rarity_value = Ball.Rarity.keys()[rarity]
+		var rarity_value = BallEffect.RARITY_TEXT[rarity]
 		var rarity_color: Color = Ball.BALL_RARITY_COLORS[rarity]
 		var rarity_color_code = rarity_color.to_html()
-		description += "[color=%s]%s[/color]" % [rarity_color_code, rarity_value]
-		description += " \n"
-		description += BallEffect.get_effect_description(ball.level, rarity)
-		description += " \n \n"
+		description += "[color=%s][b]%s[/b][/color]\n" % [rarity_color_code, rarity_value]
+		description += "%s\n" % [BallEffect.get_effect_description(ball.level, rarity)]
+		description += "[font_size=8] [/font_size]\n"
 	_ball_popup_description.text = description
 
 func _hide_ball_popup(ball: Ball) -> void:
