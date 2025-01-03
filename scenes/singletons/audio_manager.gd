a496485e65a2eb9d4ca6648f4546804bd8ac0374
Kamiko Loss <info@gloxi.net>
2025-01-03 13:52:42 +0900

    オプション画面で音量を 0 にしても消えない不具合を修正した

--- scenes/singletons/audio_manager.gd
+++ scenes/singletons/audio_manager.gd
@@ -46,9 +46,11 @@ func initialize() -> void:
 # 0: -40db, 5: -20db, 10: 0db, 
 func set_volume(bus_type: BusType, volume_level: int) -> void:
 	print("[AudioManager] set_volume(bus_type: %s, volume_level: %s)" % [bus_type, volume_level])
-	var volume_clamped = clampi(volume_level, 0, 10)
-	var volume_db = -40 + volume_clamped * 4
+	var volume_level_clamped = clampi(volume_level, 0, 10)
+	var volume_db = -40 + volume_level_clamped * 4
+	var is_mute = volume_level_clamped == 0
 	AudioServer.set_bus_volume_db(bus_type, volume_db)
+	AudioServer.set_bus_mute(bus_type, is_mute)
 
 
 func play_se(se_type: SeType) -> void:
