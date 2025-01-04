ccd9aa8ac8dfc6f274de4d03b4989a55d30bbcab
Kamiko Loss <info@gloxi.net>
2025-01-04 23:09:42 +0900

    Promise 修正 (途中)

--- scenes/utils/promise.gd
+++ scenes/utils/promise.gd
@@ -3,43 +3,30 @@ class_name Promise
 # 複数の Signal を一斉に実行するクラス
 
 
-signal completed_all(args)
-signal completed_any(args)
+# すべての Signal が発火したときに発火する
+signal completed_all
+# いずれかの Signal が発火したときに発火する
+signal completed_any
 
 
-var _signals: Dictionary = {} # signal: { has_emitted: bool, data: any }
-var _completed: bool = false
+# { <Signal>: { "emitted": bool <発火したかどうか>, "data": [ <返り値1>, ... ] } }
+var _signals: Dictionary = {}
 
 
 func _init(signals: Array[Signal]) -> void:
 	for s in signals:
-		_signals[s] = s
-		s.connect(_on_signal)
+		_signals[s] = { "emitted": false, "data": [] }
+		s.connect(_on_signal_emitted.bind(s))
 
 
-func _on_signal(s: Signal) -> void:
-	_signals[s].has_emitted = true
-	_check_completion()
-
-
-func _check_completion():
-	if _completed:
-		return
-	_check_all_completion()
-	_check_any_completion()
-
-
-func _check_all_completion() -> void:
-	var return_data: Array = []
-	for s in _signals:
-		if not _signals[s].has_emitted:
-			return
-		else:
-			return_data.append(_signals[s].data)
-
-
-func _check_any_completion() -> void:
-	for s in _signals:
-		if _signals[s].has_emitted:
-			completed_all.emit(_signals[s].data)
-			return
+func _on_signal_emitted(s: Signal) -> void:
+	_signals[s]["emitted"] = true
+	#_signals[s]["data"] = [arg1, arg2, arg3, arg4]
+	if _signals.values().any(func(v): v["emitted"]):
+		completed_any.emit()
+		print("[Promise] completed_any")
+		#completed_any.emit(_signals[s]["data"])
+	if _signals.values().all(func(v): v["emitted"]):
+		completed_all.emit()
+		print("[Promise] completed_all")
+		#completed_all.emit(_signals.values().map(func(v): v["data"]))
