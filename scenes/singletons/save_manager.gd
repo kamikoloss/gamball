840126b4a95f94ba67f122751c59ff79a58bbd0e
Kamiko Loss <info@gloxi.net>
2025-01-19 02:08:26 +0900

    Game Config を保存するようにした

--- scenes/singletons/save_manager.gd
+++ scenes/singletons/save_manager.gd
@@ -3,10 +3,12 @@ extends Node
 
 
 const CONFIG_FILE_PATH = "user://config"
+const CONFIG_KEY_GAME = "game"
 const CONFIG_KEY_VIDEO = "video"
 const CONFIG_KEY_AUDIO = "audio"
 
 
+var game_config: GameConfig
 var video_config: VideoConfig
 var audio_config: AudioConfig
 
@@ -17,6 +19,8 @@ func _ready() -> void:
 
 
 func init_save_data() -> void:
+	if not game_config:
+		game_config = GameConfig.new()
 	if not video_config:
 		video_config = VideoConfig.new()
 	if not audio_config:
@@ -30,6 +34,8 @@ func load_config() -> void:
 	var config_data = JSON.parse_string(config_file.get_line())
 	for config_key in config_data.keys():
 		match config_key:
+			CONFIG_KEY_GAME:
+				game_config.deserialize(config_data[CONFIG_KEY_GAME])
 			CONFIG_KEY_VIDEO:
 				video_config.deserialize(config_data[CONFIG_KEY_VIDEO])
 			CONFIG_KEY_AUDIO:
@@ -40,6 +46,7 @@ func load_config() -> void:
 func save_config() -> void:
 	var config_file = FileAccess.open(CONFIG_FILE_PATH, FileAccess.WRITE)
 	var config_data = JSON.stringify({
+		CONFIG_KEY_GAME: game_config.serialize(),
 		CONFIG_KEY_VIDEO: video_config.serialize(),
 		CONFIG_KEY_AUDIO: audio_config.serialize(),
 	})
@@ -58,6 +65,12 @@ class SaveDataBase:
 			set(property_name, dict[property_name])
 	func get_peroperty_names() -> Array[String]:
 		return []
+
+
+class GameConfig extends SaveDataBase:
+	var language := "ja"
+	func get_peroperty_names() -> Array[String]:
+		return ["language"]
 
 
 class VideoConfig extends SaveDataBase:
