990ce3c82c614eb3ec63c7935779f6e1cbddf6bb
Kamiko Loss <info@gloxi.net>
2025-01-18 22:53:25 +0900

    Selector に Lamp を使わないようにした

--- scenes/common/ui/selector.gd
+++ scenes/common/ui/selector.gd
@@ -9,7 +9,7 @@ signal changed # (option_key: String)
 @export var _left_button: Button
 @export var _right_button: Button
 @export var _value_label: Label
-@export var _lamps_parent: Control
+@export var _points_parent: Control
 
 
 var options: Dictionary = {}:
@@ -24,8 +24,8 @@ var _selected_option_index: int = 0
 
 
 func _ready() -> void:
-	#options = { "k1": "v1", "k2": "v2", "k3": "v3" } # Debug
-	#default_option_key = "k2" # Debug
+	options = { "k1": "v1", "k2": "v2", "k3": "v3" } # Debug
+	default_option_key = "k2" # Debug
 
 	_left_button.pressed.connect(func(): _shift_option(-1))
 	_right_button.pressed.connect(func(): _shift_option(1))
@@ -42,17 +42,16 @@ func _refresh_view() -> void:
 	_value_label.text = options[_option_keys[_selected_option_index]]
 
 	# Lamp
-	var lamp_index: int = 0
-	for lamp in _lamps_parent.get_children():
-		if lamp is Lamp:
-			if lamp_index < _option_keys.size():
-				if lamp_index == _selected_option_index:
-					lamp.enable()
-				else:
-					lamp.disable()
+	var point_index: int = 0
+	for point: Control in _points_parent.get_children():
+		if point_index < _option_keys.size():
+			if point_index == _selected_option_index:
+				point.modulate = ColorData.PRIMARY
 			else:
-				lamp.visible = false
-			lamp_index += 1
+				point.modulate = ColorData.GRAY_60
+		else:
+			point.visible = false
+		point_index += 1
 
 
 func _set_option(option_key: String) -> void:
